f1d4a99beebcbdeba4b615b96dcbe70e9b9d43f8
WakanDoo <doquockhanh308@gmail.com>
2026-04-30 02:27:29 +0700

    deploy github pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+
+
+  basePath: isProd ? "/nhom11_noithat" : "",
+  assetPrefix: isProd ? "/nhom11_noithat/" : "",
+
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        pathname: "/**",
-      },
-    ],
   },
 };
 
